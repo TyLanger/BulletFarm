1f0f6591339119941ab242133dcc045bd845ef3e
TyLanger <ty.langer16@gmail.com>
2018-09-01 00:41:21 -0600

    Added Room Art
    Added more hardwood floor variations. Created FloorBuilder to place the
    floor tiles. Tried to make FloorBuilder create the floor while you're in
    the editor, but it doesn't quite work. When you change something, it
    creates a new floor, but doesn't destroy the old floor.
    
    Added a bar stool, table, pillar and walls at the top.

---
+++ Assets/Scripts/FloorBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBuilder : MonoBehaviour {
+
+
+    public GameObject[] floorTiles;
+
+    public int xSize;
+    public int ySize;
+
+    public float xSpacing;
+    public float ySpacing;
+
+    Transform parent;
+
+	// Use this for initialization
+	void Start () {
+        InitializeFloor();
+        BuildFloor();
+	}
+
+    
+    private void OnValidate()
+    {
+        InitializeFloor();
+        BuildFloor();
+    }
+
+    void InitializeFloor()
+    {
+        if(parent != null)
+        {
+            Destroy(parent.gameObject);
+        }
+        parent = new GameObject("FloorTilesParent").transform;
+        parent.parent = transform;
+        
+    }
+
+    void BuildFloor()
+    {
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                
+                Instantiate(floorTiles[Random.Range(0, floorTiles.Length-1)], transform.position + new Vector3(i * xSpacing, j * ySpacing, 0), Quaternion.identity, parent);
+            }
+        }
+    }
+}
