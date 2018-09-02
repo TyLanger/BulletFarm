7e72d4fe0777b2dbabb96f989d7a7d524efcd5d0
TyLanger <ty.langer16@gmail.com>
2018-09-02 13:14:17 -0600

    Title Screen
    Spell the title out by shooting holes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+
+    public GameObject[] menuShots;
+    AudioSource aSource;
+
+	// Use this for initialization
+	void Start () {
+        aSource = GetComponent<AudioSource>();
+        StartCoroutine(ShootTitle());
+	}
+	
+	void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // menu is sceen 0
+            // game is scene 1
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    IEnumerator ShootTitle()
+    {
+        for (int i = 0; i < menuShots.Length; i++)
+        {
+            menuShots[i].transform.Rotate(Vector3.forward, Random.Range(0, 3) * 90);
+            menuShots[i].SetActive(true);
+            /*
+            if (!aSource.isPlaying)
+            {
+                aSource.Play();
+            }
+            */
+            aSource.Play();
+
+            yield return new WaitForSeconds(0.09f);
+
+        }
+
+
+    }
+}
